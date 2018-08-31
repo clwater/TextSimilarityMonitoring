3db7d8a9d7963be431ab9f1b131e0766ff6f91df
馍 <mo@lingyongqian.cn>
2018-08-31 11:06:38 +0800

    feat(生成文章摘要):

--- abstract.py
+++ abstract.py
@@ -6,22 +6,21 @@ from textrank4zh import TextRank4Keyword, TextRank4Sentence
 
 
 def getAbs(woldFile):
-    print('woldFile path:  ' + woldFile)
-    with open(woldFile, 'r') as f:
-        text = f.read()
-        # print (sequence)
-        tr4w = TextRank4Keyword()
-        tr4w.analyze(text=text, lower=True,
-                     window=2)  # py2中text必须是utf8编码的str或者unicode对象，py3中必须是utf8编码的bytes或者str对象
-        tr4s = TextRank4Sentence()
-        tr4s.analyze(text=text, lower=True, source='all_filters')
-        print('摘要：')
-        # item = tr4s[0]
-        # print(item.index, item.weight, item.sentence)
-        # for item in tr4s.get_key_sentences(num=1):
-        #     print(item.index, item.weight)
-        #     print(item.sentence)
+    try:
+        with open(woldFile, 'r') as f:
+            text = f.read()
+            # print (sequence)
+            tr4w = TextRank4Keyword()
+            tr4w.analyze(text=text, lower=True,
+                         window=2)  # py2中text必须是utf8编码的str或者unicode对象，py3中必须是utf8编码的bytes或者str对象
+            tr4s = TextRank4Sentence()
+            tr4s.analyze(text=text, lower=True, source='all_filters')
+            for item in tr4s.get_key_sentences(num=1):
+                # print(item.index, item.weight)
+                # print(item.sentence)
+                with open('./cache/absCache', 'a') as f:
+                    f.write(woldFile + ' ||| ' +item.sentence +'\n')
+                print(woldFile + 'build abstract success')
+    except:
+        print(woldFile + 'build abstract error')
 
-        item = tr4s.get_key_sentences(num=1)
-        print(item.index, item.weight)
-        print(item.sentence)
--- world.py
+++ world.py
@@ -3,8 +3,17 @@
 import os
 import abstract
 
+def cleanAbsCache():
+    print('clean abstract cache')
+    with open('./cache/absCache', 'w') as f:
+        f.write('')
+
+
 def initWorld():
     print('============')
+
+    cleanAbsCache()
+
     print('initWorld')
     # 获取paper文件夹下面所有的文件名
     filelist = os.listdir('./paper')
